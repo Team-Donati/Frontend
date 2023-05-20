c311d806a694ddd18ec8a2404e0a4b51369cf9b2
허원석 <1sucker@heowonseog-ui-MacBookPro.local>
2023-05-21 05:10:47 +0900

    리얼 끝

--- src/pages/letter.tsx
+++ src/pages/letter.tsx
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from "react";
 import ClaimInfos from "../components/claimInfos";
 
 const Letter = () => {
-  return <div>letter page</div>;
+  return (
+    <div id="mainwrap">
+      <div className="main">
+        <div className="blank"></div>
+        <a href="http://localhost:3000">
+          <div className="depth_nav"></div>
+        </a>
+        <div className="letter_send_img"></div>
+        <input
+          type="text"
+          id="letter_input"
+          name="letter_input"
+          placeholder="To. my donor,"
+        ></input>
+      </div>
+    </div>
+  );
 };
 
 export default Letter;
