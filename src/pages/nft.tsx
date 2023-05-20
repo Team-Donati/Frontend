67427dfd002bf47171e9396b989bb2667650e45e
허원석 <1sucker@heowonseog-ui-MacBookPro.local>
2023-05-21 04:26:46 +0900

    nft page 완료

--- src/pages/nft.tsx
+++ src/pages/nft.tsx
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 const Nft = () => {
-  return <div>nft page</div>;
+  return (
+    <div id="mainwrap">
+      <div className="main">
+        <div className="blank"></div>
+        <a href="http://localhost:3000">
+          <div className="depth_nav"></div>
+        </a>
+        <div className="nft_filter"></div>
+        <div className="nft_letter_wrap">
+          <div className="nft_letter"></div>
+        </div>
+
+        <div className="homein"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Nft;
