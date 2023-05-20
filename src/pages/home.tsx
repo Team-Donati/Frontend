35b2d724e46c3ee05090a8d645df889307886870
허원석 <1sucker@heowonseog-ui-MacBookPro.local>
2023-05-21 00:04:58 +0900

    home.tsx 완료

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,31 @@
+import "./main.css";
 const Home = ({ connectWallet }: any) => {
   return (
-    <div>
-      Home Page
-      <button onClick={connectWallet}> click</button>
+    <div id="mainwrap">
+      <div className="main">
+        <div className="blank"></div>
+        <div className="nav"></div>
+        <div className="position_banner"></div>
+        <a href="http://localhost:3000/explore">
+          <div className="explore_btn"></div>
+        </a>
+        <div className="explore_menu"></div>
+        <a href="http://localhost:3000/explore">
+          <div className="explore_banner"></div>
+        </a>
+        <div className="bottom_nav">
+          <a href="http://localhost:3000/explore">
+            <div className="nav_01"></div>
+          </a>
+          <a href="http://localhost:3000/nft">
+            <div className="nav_02"></div>
+          </a>
+          <a href="http://localhost:3000/donate">
+            <div className="nav_03"></div>
+          </a>
+        </div>
+        <div className="homein"></div>
+      </div>
     </div>
   );
 };
